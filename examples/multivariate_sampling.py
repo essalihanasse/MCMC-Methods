1d6a697eccc86e34301f09a9dacfc9f3892f688c
essalihanasse <essalihanasse5@gmail.com>
2024-10-21 22:49:39 +0100

    Test commit

--- examples/multivariate_sampling.py
+++ examples/multivariate_sampling.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+# Add the parent directory to sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
 from mcmc_samplers.multivariate_sampler import MultivariateMetropolisHastingsSampler
 import numpy as np
 import matplotlib.pyplot as plt
--- examples/univariate_sampling.py
+++ examples/univariate_sampling.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+# Add the parent directory to sys.path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
 from mcmc_samplers.base_sampler import MetropolisHastingsSampler
 from mcmc_samplers.visualization import SamplerVisualizer
 import numpy as np
